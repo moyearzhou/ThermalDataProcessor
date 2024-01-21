685f39bd5209d739236301a674ad6827ca2986fb
Moyear <moyearzhou@qq.com>
2024-01-21 23:07:01 +0800

    2024.01.21 23:06 提取染色法测流速的轮廓.part

--- thermal_dat/test/blue_contours_extract.py
+++ thermal_dat/test/blue_contours_extract.py
@@ -10,10 +10,10 @@ from matplotlib import pyplot as plt
 # Load the image
 file_path = r'../res/blue_contour.png'
 
+video_path = r"E:\Moyear\文档\冲刷实验\测试数据\轨迹.mp4"
 
-def find_largest_blue_contour(image_path):
-    # Read the image
-    image = cv2.imread(image_path)
+
+def find_largest_blue_contour(image):
 
     # Convert the image to RGB (OpenCV uses BGR by default)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -22,16 +22,21 @@ def find_largest_blue_contour(image_path):
     image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     # Define the range of blue color in HSV
-    lower_blue = np.array([100, 5, 5])
+    lower_blue = np.array([100, 40, 10])
     upper_blue = np.array([150, 255, 255])
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(image_hsv, lower_blue, upper_blue)
 
+    # cv2.imshow("mask", mask)
+    # cv2.waitKey(0)
+
     # Perform a series of erosions and dilations on the mask to remove small blobs
     kernel = np.ones((3, 3), np.uint8)
-    mask = cv2.erode(mask, kernel, iterations=2)
-    mask = cv2.dilate(mask, kernel, iterations=4)
+    mask = cv2.erode(mask, kernel, iterations=3)
+    mask = cv2.dilate(mask, kernel, iterations=3)
+
+    cv2.imshow("mask", mask)
 
     # Find contours in the masked image
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -41,18 +46,46 @@ def find_largest_blue_contour(image_path):
 
     # Draw the largest contour on the original image
     contour_image = image_rgb.copy()
-    if largest_contour is not None:
-        cv2.drawContours(contour_image, [largest_contour], -1, (255, 0, 0), 2)
+    cv2.drawContours(contour_image, contours, -1, (255, 0, 0), 2)
+
+    # if largest_contour is not None:
+    #     cv2.drawContours(contour_image, [largest_contour], -1, (255, 0, 0), 2)
 
     return contour_image
 
-# Process the image and find the largest blue contour
-contour_image = find_largest_blue_contour(file_path)
 
-# Convert the contour image to RGB for visualization
-contour_image_rgb = cv2.cvtColor(contour_image, cv2.COLOR_BGR2RGB)
+def pic_extract():
+    # Read the image
+    image = cv2.imread(file_path)
 
-# Display the image with the largest contour
-plt.imshow(contour_image_rgb)
-plt.axis('off')
-plt.show()
+    # Process the image and find the largest blue contour
+    contour_image = find_largest_blue_contour(image)
+
+    cv2.imshow("img", contour_image)
+    cv2.waitKey(0)
+
+    # Convert the contour image to RGB for visualization
+    # contour_image_rgb = cv2.cvtColor(contour_image, cv2.COLOR_BGR2RGB)
+
+    # Display the image with the largest contour
+    # plt.imshow(contour_image_rgb)
+    # plt.axis('off')
+    # plt.show()
+
+
+def video_extract():
+    cap = cv2.VideoCapture(video_path)
+    # 逐帧处理视频4321    ·
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        cv2.imshow('ori', frame)
+        img = find_largest_blue_contour(frame)
+        # img = frame
+        cv2.imshow("img", img)
+        cv2.waitKey(int(1000 / 25))
+
+
+video_extract()
