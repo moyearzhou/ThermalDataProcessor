2824254ae81578fdecd83eee32e17c3c6daae44c
Honor <moyearzhou@qq.com>
2024-01-22 02:37:05 +0800

    2024.01.22 02:36 实现部分测流速的功能

--- thermal_dat/test/blue_contours_extract.py
+++ thermal_dat/test/blue_contours_extract.py
@@ -9,28 +9,41 @@ from matplotlib import pyplot as plt
 
 # Load the image
 file_path = r'../res/blue_contour.png'
-
 video_path = r"E:\Moyear\文档\冲刷实验\测试数据\轨迹.mp4"
 
+fps = 25
 
-def find_largest_blue_contour(image):
+frame_width = -1
+frame_height = -1
 
-    # Convert the image to RGB (OpenCV uses BGR by default)
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# 槽子的长度为160cm
+real_height = 1600
+# 槽子的宽度40cm
+real_width = 400
 
+# 像素到毫米的比例，根据实际情况调整
+pixel_to_mm = -1
+
+
+def mask_by_hsv(image):
+    '''
+    提取坡面中的蓝色部分区域
+    :param image:
+    :return:
+    '''
     # Convert the image to HSV (Hue, Saturation, Value) color space
     image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     # Define the range of blue color in HSV
-    lower_blue = np.array([100, 40, 10])
+    lower_blue = np.array([100, 30, 10])
     upper_blue = np.array([150, 255, 255])
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(image_hsv, lower_blue, upper_blue)
+    return mask
 
-    # cv2.imshow("mask", mask)
-    # cv2.waitKey(0)
 
+def find_blue_contours(mask):
     # Perform a series of erosions and dilations on the mask to remove small blobs
     kernel = np.ones((3, 3), np.uint8)
     mask = cv2.erode(mask, kernel, iterations=3)
@@ -41,51 +54,136 @@ def find_largest_blue_contour(image):
     # Find contours in the masked image
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # todo 对这些轮廓进行一定过滤
+
     # Find the largest contour by area
     largest_contour = max(contours, key=cv2.contourArea) if contours else None
-
-    # Draw the largest contour on the original image
-    contour_image = image_rgb.copy()
-    cv2.drawContours(contour_image, contours, -1, (255, 0, 0), 2)
-
-    # if largest_contour is not None:
-    #     cv2.drawContours(contour_image, [largest_contour], -1, (255, 0, 0), 2)
-
-    return contour_image
+    return contours
 
 
-def pic_extract():
-    # Read the image
-    image = cv2.imread(file_path)
+def get_center_point(contour):
+    M = cv2.moments(contour)
 
-    # Process the image and find the largest blue contour
-    contour_image = find_largest_blue_contour(image)
-
-    cv2.imshow("img", contour_image)
-    cv2.waitKey(0)
-
-    # Convert the contour image to RGB for visualization
-    # contour_image_rgb = cv2.cvtColor(contour_image, cv2.COLOR_BGR2RGB)
-
-    # Display the image with the largest contour
-    # plt.imshow(contour_image_rgb)
-    # plt.axis('off')
-    # plt.show()
+    center = (-1, -1)
+    if M['m00'] != 0:
+        center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
+    return center
 
 
 def video_extract():
     cap = cv2.VideoCapture(video_path)
+
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    print('视频宽：{0}，高：{1}'.format(frame_width, frame_height))
+    pixel_to_mm = real_height / frame_height
+
+    print("在纵向上，每个像素为{0}mm".format(pixel_to_mm))
+
+    # 示踪剂前缘点的轮廓
+    pre_contour = None
+
+    # 开始滴水的帧索引
+    start_frame_index = 0
+
+    frame_index = 0
     # 逐帧处理视频4321    ·
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
+        frame_index += 1
+
+        # 对视频进行一定的旋转操作，使水流自上而下流动
+        frame = cv2.rotate(frame, cv2.ROTATE_180)
+
         cv2.imshow('ori', frame)
-        img = find_largest_blue_contour(frame)
+        # 根据蓝色提取出坡面中可能为染色的区域
+        mask = mask_by_hsv(frame)
+        # 通过形态学操作，过滤出蓝色流体的区域
+        contours = find_blue_contours(mask)
+
+        # Draw the largest contour on the original image
+        contour_image = frame.copy()
+        cv2.drawContours(contour_image, contours, -1, (0, 0, 255), 2)
+
+        # todo 根据轮廓计算流速
+        # 计算流速
+        if len(contours) > 0:
+            # 初始化最大y坐标和对应的轮廓索引
+            max_y = -1
+            max_contour_index = -1
+
+            # 遍历轮廓
+            for i, contour in enumerate(contours):
+                # 计算当前轮廓的边界框
+                x, y, w, h = cv2.boundingRect(contour)
+                # 获取当前轮廓的y坐标
+                contour_y = y + h
+
+                # 检查当前轮廓的y坐标是否大于最大y坐标
+                if contour_y > max_y:
+                    max_y = contour_y
+                    max_contour_index = i
+
+            max_contour = None
+            # 检查是否找到了具有最大y坐标的轮廓
+            if max_contour_index != -1:
+                # 获取最大y坐标对应的轮廓
+                max_contour = contours[max_contour_index]
+
+                # 计算最大y坐标对应轮廓的边界框
+                x, y, w, h = cv2.boundingRect(max_contour)
+
+                # 获取最大y坐标对应轮廓的x和y坐标值
+                contour_x = x
+                contour_y = y + h
+
+                # 打印结果
+                # print("最大y坐标的轮廓的x坐标：", contour_x)
+                # print("最大y坐标的轮廓的y坐标：", contour_y)
+            else:
+                print("未找到轮廓")
+
+            if pre_contour is None:
+                pre_contour = max_contour
+                start_frame_index = frame_index
+            else:
+                pre_center = get_center_point(pre_contour)
+                cur_center = get_center_point(max_contour)
+
+                x0 = pre_center[0]
+                x1 = cur_center[0]
+
+                y0 = pre_center[1]
+                y1 = cur_center[1]
+
+                displacement = np.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+                real_distance = displacement * pixel_to_mm  # 计算真实移动的距离，单位mm
+
+                # print(real_distance)
+
+                if real_distance >= 100:
+                    time_interval = (frame_index - start_frame_index) / fps  # 时间间隔，单位为ms
+
+                    flow_speed = real_distance / time_interval  # 计算流速，单位：mm/ms即是m/s
+                    print('Flow Speed:', flow_speed, "m/s")
+
+                    # todo 如果在它前面则更新
+                    # if y1 > y0:
+                    pre_contour = max_contour
+                    start_frame_index = frame_index
+
+
+        # print(contours)
+
+        # if largest_contour is not None:
+        #     cv2.drawContours(contour_image, [largest_contour], -1, (255, 0, 0), 2)
+
         # img = frame
-        cv2.imshow("img", img)
-        cv2.waitKey(int(1000 / 25))
+        cv2.imshow("img", contour_image)
+        cv2.waitKey(int(1000 / fps))
 
 
 video_extract()
