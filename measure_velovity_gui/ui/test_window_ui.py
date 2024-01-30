77d125ef338aedd3d460818395a24d5f596adf9b
Honor <moyearzhou@qq.com>
2024-01-30 08:54:15 +0800

    实现的v14的适配

---
+++ measure_velovity_gui/ui/test_window_ui.py
@@ -0,0 +1,24 @@
+import sys
+
+from PyQt5 import QtCore, QtWidgets
+
+from measure_velovity_gui.ui.measure_velocity import Ui_MainWindow
+
+# 设置异常处理钩子
+# sys.excepthook = handle_exception
+
+# 高分辨率适配
+QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+
+my_app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+# 绑定监听器
+# ui.bindListener(MainWindow)
+
+# 初始化界面
+# ui.init_view(MainWindow)
+
+MainWindow.show()
+sys.exit(my_app.exec_())
